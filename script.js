8fc1078bb1abf891af9079ce7002d8d89101be42
Leandro Perego <leperego@hotmail.com>
2023-03-02 20:29:12 -0300

    Finalizado primeira versao canais de venda

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let nav = document.querySelector("nav");
 let span_icon_chevron = document.querySelector("#span-icon-chevron");
 let icon_menu = document.querySelector("#icon-menu");
+let btn_shopee = document.querySelector("#btn-canais-venda");
 
 nav.addEventListener(`click`, showSubMenu);
+btn_shopee.addEventListener('click', () => {
+    window.open("https://shopee.com.br/kapepapelaria", "_blank");
+});
 
 function showSubMenu(){
     nav.classList.toggle("min-height-submenu");
