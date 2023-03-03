0697d8806e0bd0e8dfffe77135c47da080838ef4
Leandro Perego <leperego@hotmail.com>
2023-03-03 13:20:52 -0300

    Criado primeira versao produtos mais vendidos

--- script.js
+++ script.js
@@ -20,12 +20,37 @@ function showSubMenu(){
 }
 
 // CARROSSEL
+// const carousel = document.querySelector('.carousel');
+// const carouselContainer = carousel.querySelector('.carousel-container');
+// const carouselItems = carousel.querySelectorAll('.carousel-item');
+
+// let currentIndex = 0;
+// let translateX = 0;
+
+// function slideCarousel() {
+//   currentIndex++;
+
+//   if (currentIndex > carouselItems.length - 1) {
+//     currentIndex = 0;
+//   }
+
+//   translateX = -currentIndex * carouselItems[0].offsetWidth;
+
+//   carouselContainer.style.transform = `translateX(${translateX}px)`;
+// }
+
+// setInterval(slideCarousel, 3000);
+
+// CARROSSEL MOBILE
 const carousel = document.querySelector('.carousel');
 const carouselContainer = carousel.querySelector('.carousel-container');
 const carouselItems = carousel.querySelectorAll('.carousel-item');
 
 let currentIndex = 0;
-let translateX = 0;
+let startX = 0;
+let currentTranslateX = 0;
+let previousTranslateX = 0;
+let dragging = false;
 
 function slideCarousel() {
   currentIndex++;
@@ -34,9 +59,50 @@ function slideCarousel() {
     currentIndex = 0;
   }
 
-  translateX = -currentIndex * carouselItems[0].offsetWidth;
+  currentTranslateX = -currentIndex * carousel.clientWidth;
 
-  carouselContainer.style.transform = `translateX(${translateX}px)`;
+  carouselContainer.style.transform = `translate3d(${currentTranslateX}px, 0, 0)`;
 }
 
-// setInterval(slideCarousel, 3000);
+function startDrag(event) {
+  event.preventDefault();
+
+  dragging = true;
+  startX = event.touches[0].clientX;
+  previousTranslateX = currentTranslateX;
+}
+
+function drag(event) {
+  event.preventDefault();
+
+  if (dragging) {
+    const currentX = event.touches[0].clientX;
+    const deltaX = currentX - startX;
+    currentTranslateX = previousTranslateX + deltaX;
+
+    carouselContainer.style.transform = `translate3d(${currentTranslateX}px, 0, 0)`;
+  }
+}
+
+function endDrag(event) {
+  event.preventDefault();
+
+  dragging = false;
+
+  const threshold = carousel.clientWidth / 3;
+  const deltaX = currentTranslateX - previousTranslateX;
+
+  if (deltaX > threshold && currentIndex > 0) {
+    currentIndex--;
+  } else if (deltaX < -threshold && currentIndex < carouselItems.length - 1) {
+    currentIndex++;
+  }
+
+  slideCarousel();
+}
+
+carousel.addEventListener('touchstart', startDrag);
+carousel.addEventListener('touchmove', drag);
+carousel.addEventListener('touchend', endDrag);
+carousel.addEventListener('touchcancel', endDrag);
+
