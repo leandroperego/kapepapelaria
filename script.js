1aedee80707d45c807c9cc7caaadeac32c425009
Leandro Perego <leperego@hotmail.com>
2023-03-03 20:39:16 -0300

    Criado movimento touch sem tamanho fixo

--- script.js
+++ script.js
@@ -20,25 +20,100 @@ function showSubMenu(){
 }
 
 // CARROSSEL
-const carousel = document.querySelector('.carousel');
-const carouselContainer = carousel.querySelector('.carousel-container');
-const carouselItems = carousel.querySelectorAll('.carousel-item');
+// const carousel = document.querySelector('.carousel');
+// const carouselContainer = carousel.querySelector('.carousel-container');
+// const carouselItems = carousel.querySelectorAll('.carousel-item');
 
+// let currentIndex = 0;
+// let translateX = 0;
+
+// function slideCarousel() {
+//   currentIndex++;
+
+//   if (currentIndex > carouselItems.length - 1) {
+//     currentIndex = 0;
+//   }
+
+//   translateX = -currentIndex * carouselItems[0].offsetWidth;
+
+//   carouselContainer.style.transform = `translateX(${translateX}px)`;
+// }
+
+// setInterval(slideCarousel, 3000);
+
+// PERCORRER PRODUTOS + VENDIDOS
+const cardsContainer = document.querySelector('.cards-container');
+let isDragging = false;
+let startPosition = 0;
+let currentTranslate = 0;
+let prevTranslate = 0;
+let animationId = 0;
 let currentIndex = 0;
-let translateX = 0;
 
-function slideCarousel() {
-  currentIndex++;
+function onTouchStart(e) {
+  currentIndex = Array.from(cardsContainer.children).indexOf(e.target);
+  startPosition = e.touches[0].clientX;
+  isDragging = true;
 
-  if (currentIndex > carouselItems.length - 1) {
-    currentIndex = 0;
-  }
-
-  translateX = -currentIndex * carouselItems[0].offsetWidth;
-
-  carouselContainer.style.transform = `translateX(${translateX}px)`;
+  // interrompe a animação atual, se houver
+  cancelAnimationFrame(animationId);
 }
 
-setInterval(slideCarousel, 3000);
+function onTouchMove(e) {
+  if (isDragging) {
+    const currentPosition = e.touches[0].clientX;
+    currentTranslate = prevTranslate + currentPosition - startPosition;
+  }
+}
+
+function onTouchEnd() {
+  isDragging = false;
+
+  // determina para qual card o usuário "arrastou" e ajusta o deslocamento
+  const cards = Array.from(cardsContainer.children);
+  const cardWidth = cards[0].getBoundingClientRect().width;
+  const cardTranslate = currentTranslate / cardWidth;
+  const roundCardTranslate = Math.round(cardTranslate);
+
+  if (roundCardTranslate < 0) {
+    currentIndex = 0;
+  } else if (roundCardTranslate >= cards.length - 1) {
+    currentIndex = cards.length - 1;
+  } else {
+    currentIndex = roundCardTranslate;
+  }
+
+  currentTranslate = currentIndex * -cardWidth;
+  prevTranslate = currentTranslate;
+
+  // anima o scroll para o card selecionado
+  requestAnimationFrame(() => {
+    animateScroll();
+  });
+}
+
+function animateScroll() {
+  cardsContainer.style.transform = `translateX(${currentTranslate}px)`;
+
+  // atualiza a posição do card atual
+  const cards = Array.from(cardsContainer.children);
+  cards.forEach((card, index) => {
+    if (index === currentIndex) {
+      card.classList.add('active');
+    } else {
+      card.classList.remove('active');
+    }
+  });
+
+  // requisita animação recursiva até o deslocamento atual ser igual ao deslocamento desejado
+  if (currentTranslate !== prevTranslate) {
+    animationId = requestAnimationFrame(animateScroll);
+  }
+}
+
+cardsContainer.addEventListener('touchstart', onTouchStart);
+cardsContainer.addEventListener('touchmove', onTouchMove);
+cardsContainer.addEventListener('touchend', onTouchEnd);
+
 
 
