bc0ca68ab6cc34a23af961827b2f5ae1ae96c62d
Leandro Perego <leperego@hotmail.com>
2023-03-10 19:49:29 -0300

    retirado evento resize para o vh

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ let vh = window.innerHeight * 0.01;
 
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-window.addEventListener('resize', () =>{
-  vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
 // ------------------------------------------------
 
 
